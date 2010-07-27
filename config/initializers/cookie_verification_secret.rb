4ed129a71735c5677052f22a36932aef014e5f24
Joe <thelechner@gmail.com>
2010-07-28 01:42:58 +0200

    Initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'b20a57225a364218ad1bfe2fe91822ca5d3d2210a37cfe72585a5d87bc1adf086cee06edb8c71741e644d57b3a2f77d4d614f9a64d032452707d36590f03f248';
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_first_app_session',
+  :secret      => 'a78424360bb39949036b93d805bff813a4138ed2a495380cc1f5561221944329bb5a03d5bdaa7a5dafc8917136d687b416125cca5b99e557cff1b122ca8148e5'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
